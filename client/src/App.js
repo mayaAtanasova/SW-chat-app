94bb664ec256d7a76640326aea1f5cde974bc09f
Maya Atanasova <mayaatanasova@gmail.com>
2022-05-21 16:37:03 +0300

    before unload handler

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,8 @@ import JoinForm from './components/JoinForm/JoinForm';
 import ChatForm from './components/Chat/ChatForm';
 import ChatContext from './store/chat-context';
 
-const socket = io.connect('http://localhost:3001');
+// const socket = io.connect('http://localhost:3001');
+const socket = io.connect('https://streamworks-chat.herokuapp.com/');
 
 function App() {
 
